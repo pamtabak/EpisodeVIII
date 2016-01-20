d2b912d819e94fb68f11f14e4365ed984dc5755e
pamtabak <pamelatabak@poli.ufrj.br>
2016-01-20 18:51:02 -0200

    introducted a long timeago phrase

--- js/intro.js
+++ js/intro.js
@@ -1,81 +1,264 @@
-var scene, camera, renderer;
-var backgroundScene  = new THREE.Scene();
-var backgroundCamera = new THREE.Camera();
+var container, stats;
+var camera, scene, renderer;
+var group, text, text2;
+var wholeText = [];
+var groups = [];
+
+var clock;
 
 init();
-render();
+animate();
 
 function init() {
 
-	// Initializing global variables
-	scene = new THREE.Scene(); // set up the scene
-	// scene.fog = new THREE.FogExp2(0xffffff, 0.0003);
-	camera = new THREE.PerspectiveCamera (75, window.innerWidth/window.innerHeight, 0.1, 1000);
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
 
-	// Create lights
-    var light = new THREE.PointLight(0xEEEEEE);
-    light.position.set(20, 0, 20);
-    scene.add(light);
+	// Initializing scene
+	scene = new THREE.Scene();
 
-    var lightAmb = new THREE.AmbientLight(0x777777);
-    scene.add(lightAmb);
+	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
+	camera.position.set( 0, 160, 200 );
 
-	// Set up the rendered
- 	renderer = new THREE.WebGLRenderer({antialias:true, alpha: true});
- 	// renderer.setClearColor(scene.fog.color);
-	renderer.setSize(window.innerWidth, window.innerHeight); // set the size at which we want it to render our app
-	document.body.appendChild(renderer.domElement);
+	// set clear color as transparent, so the background doenst disappear
+	renderer = new THREE.WebGLRenderer({antialias:true, alpha:true});
+	renderer.setSize( window.innerWidth, window.innerHeight );	
+	container.appendChild( renderer.domElement );
 
-	// Set up the main camera
-    camera.position.z = 5;
+	clock = new THREE.Clock();
 
-	initBackground();
+	createIntroText();
 
-	var theText  = "Star Wars"
-	var theText2 = "A long time ago, in a galaxy far, far away...."
-	var theText3 = "It is a period of civil war. Rebel"
-	var theText4 = "spaceships, striking from a hidden"
-	var theText5 = "base, have won their first victory"
-	var theText6 = "against the evil Galactic Empire."
+// 	var theText      = "Star Wars"
+// 	var theText2     = "Em uma sala de aula na UFRJ, em pleno verao...";
+//     var theText3     = "It is a period of civil war. Rebel"
+// 	var theText4     = "spaceships, striking from a hidden"
+// 	var theText5     = "base, have won their first victory"
+// 	var theText6     = "against the evil Galactic Empire."
 
-	// Get text from hash
-	var hash = document.location.hash.substr( 1 );
-	if ( hash.length !== 0 ) {
-		theText = hash;
-	}
+// 	// Get text from hash
+// 	var hash = document.location.hash.substr( 1 );
+// 	if ( hash.length !== 0 ) {
+// 		theText = hash;
+// 	}
 
-	var text3d2 = new THREE.TextGeometry (theText2, {
-		size: 20,
-		//height seems to change the volume of the text
-		height: 5,
-		curveSegments: 5,
-		font: "helvetiker"
-	});
+// 	//--------------------------- TEST PART --------------------------
+// 	//Adding each sentence separately cause I didn't get yet how to do it another way.
 
-	text3d2.computeBoundingBox();
-	var centerOffset = -0.5 * ( text3d2.boundingBox.max.x - text3d2.boundingBox.min.x );
-	var textMaterial2 = new THREE.MeshBasicMaterial( { color: 'rgb(253, 221, 0)', overdraw: 0.1 });
-	text2 = new THREE.Mesh( text3d2, textMaterial2 );
-	text2.position.x = centerOffset;
-	text2.position.y = 60;
-	text2.position.z = 300;
-	//Gives the text an angle
-	text2.rotation.x = 150;
-	//text2.rotation.y = Math.PI * 2;
-	group = new THREE.Object3D();
-	group.add( text2 );
-	scene.add( group );
+// 	var text3d2 = new THREE.TextGeometry( theText2, {
+// 		size: 20,
+// 		//height seems to change the volume of the text
+// 		height: 5,
+// 		curveSegments: 5,
+// 		font: "helvetiker"
+// 	});
+
+// 	text3d2.computeBoundingBox();
+// 	var centerOffset = -0.5 * ( text3d2.boundingBox.max.x - text3d2.boundingBox.min.x );
+// 	var textMaterial2 = new THREE.MeshBasicMaterial( { color: 'rgb(253, 221, 0)', overdraw: 0.1 });
+// 	text2 = new THREE.Mesh( text3d2, textMaterial2 );
+
+// 	text2.position.x = centerOffset;
+// 	text2.position.y = 60;
+// 	text2.position.z = 300;
+
+// 	//Gives the text an angle
+// 	text2.rotation.x = 150;
+// 	//text2.rotation.y = Math.PI * 2;
+
+// 	group = new THREE.Object3D();
+// 	group.add( text2 );
+// 	scene.add( group );
+
+//     //--------------------- 3rd sentence -----------------
+
+// 	var text3d3 = new THREE.TextGeometry( theText3, {
+// 		size: 18,
+// 		//height seems to change the volume of the text
+// 		height: 5,
+// 		curveSegments: 5,
+// 		font: "helvetiker"
+// 	});
+
+// 	text3d3.computeBoundingBox();
+// 	var centerOffset = -0.5 * ( text3d3.boundingBox.max.x - text3d3.boundingBox.min.x );
+// 	var textMaterial3 = new THREE.MeshBasicMaterial( { color: 'rgb(253, 221, 10)', overdraw: 0.1 });
+// 	text3 = new THREE.Mesh( text3d3, textMaterial3 );
+
+// 	text3.position.x = centerOffset;
+// 	text3.position.y = 50;
+// 	text3.position.z = 300;
+
+// 	//Gives the text an angle
+// 	text3.rotation.x = 150;
+// 	//text2.rotation.y = Math.PI * 2;
+
+// 	group = new THREE.Object3D();
+// 	group.add( text3 );
+// 	scene.add( group );
+
+// //---------------------- 4th sentence ---------------------
+// 	var text3d4 = new THREE.TextGeometry( theText4, {
+// 		size: 16,
+// 		//height seems to change the volume of the text
+// 		height: 5,
+// 		curveSegments: 5,
+// 		font: "helvetiker"
+// 	});
+
+// 	text3d4.computeBoundingBox();
+// 	var centerOffset = -0.5 * ( text3d4.boundingBox.max.x - text3d4.boundingBox.min.x );
+
+// 	var textMaterial4 = new THREE.MeshBasicMaterial( { color: 'rgb(253, 221, 10)', overdraw: 0.1 });
+// 	text4 = new THREE.Mesh( text3d4, textMaterial4 );
+
+// 	text4.position.x = centerOffset;
+// 	text4.position.y = 40;
+// 	text4.position.z = 300;
+
+// 	//Gives the text an angle
+// 	text4.rotation.x = 150;
+// 	//text2.rotation.y = Math.PI * 2;
+
+// 	group = new THREE.Object3D();
+// 	group.add( text4 );
+// 	scene.add( group );
+
+// //------------------------- 5th sentence ------------------------
+
+// 	var text3d5 = new THREE.TextGeometry( theText5, {
+// 		size: 14,
+// 		//height seems to change the volume of the text
+// 		height: 5,
+// 		curveSegments: 5,
+// 		font: "helvetiker"
+// 	});
+
+// 	text3d5.computeBoundingBox();
+// 	var centerOffset = -0.5 * ( text3d5.boundingBox.max.x - text3d5.boundingBox.min.x );
+
+// 	var textMaterial5 = new THREE.MeshBasicMaterial( { color: 'rgb(253, 221, 10)', overdraw: 0.1 });
+// 	text5 = new THREE.Mesh( text3d5, textMaterial5 );
+
+// 	text5.position.x = centerOffset;
+// 	text5.position.y = 30;
+// 	text5.position.z = 300;
+
+// 	//Gives the text an angle
+// 	text5.rotation.x = 150;
+// 	//text2.rotation.y = Math.PI * 2;
+
+// 	group = new THREE.Object3D();
+// 	group.add( text5 );
+// 	scene.add( group );
+
+// //----------------------6th sentence-----------------------------
+
+// 	var text3d6 = new THREE.TextGeometry( theText6, {
+// 		size: 12,
+// 		//height seems to change the volume of the text
+// 		height: 5,
+// 		curveSegments: 5,
+// 		font: "helvetiker"
+// 	});
+
+// 	text3d6.computeBoundingBox();
+// 	var centerOffset = -0.5 * ( text3d6.boundingBox.max.x - text3d6.boundingBox.min.x );
+
+// 	var textMaterial6 = new THREE.MeshBasicMaterial( { color: 'rgb(253, 221, 10)', overdraw: 0.1 });
+// 	text6 = new THREE.Mesh( text3d6, textMaterial6 );
+
+// 	text6.position.x = centerOffset;
+// 	text6.position.y = 20;
+// 	text6.position.z = 300;
+
+// 	//Gives the text an angle
+// 	text6.rotation.x = 150;
+// 	//text2.rotation.y = Math.PI * 2;
+
+// 	group = new THREE.Object3D();
+// 	group.add( text6 );
+// 	scene.add( group );
+
+// //---------------------------------------------------------------
+
+// 	var text3d = new THREE.TextGeometry( theText, {
+// 		size: 50,
+// 		height: 10,
+// 		curveSegments: 5,
+// 		font: "helvetiker"
+
+// 	});
+
+// 	text3d.computeBoundingBox();
+// 	var centerOffset = -0.5 * ( text3d.boundingBox.max.x - text3d.boundingBox.min.x );
+
+// 	// var textMaterial = new THREE.MeshBasicMaterial( { color: Math.random() * 0xffffff, overdraw: 0.5 } );
+// 	//The previous color of the text was randomised but I wanted it to be the Star Wars yellow.
+// 	var textMaterial = new THREE.MeshBasicMaterial( { color: 'rgb(253, 221, 0)', overdraw: 0.1 } );
+// 	//overdraw is the width of the 3D letters;
+// 	text = new THREE.Mesh( text3d, textMaterial );
+
+// 	text.position.x = centerOffset;
+// 	text.position.y = 100;
+// 	text.position.z = 200;
+
+// 	//Gives the text an angle
+// 	text.rotation.x = 150;
+// 	text.rotation.y = Math.PI * 2;
+
+// 	group = new THREE.Object3D();
+// 	group.add( text );
+
+// 	scene.add( group );
+
+	createStats();
 
 	window.addEventListener( 'resize', onWindowResize, false );
+
 }
 
-function render() {
-	requestAnimationFrame(render);
-	// camera.lookAt( scene.position );
-	renderer.autoClear = false;
-	renderer.clear();
-	//renderer.render(backgroundScene , backgroundCamera );
-	renderer.render( scene, camera );
+function createStats() {
+	stats = new Stats();
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.top = '0px';
+	container.appendChild( stats.domElement );
+}
+
+function createIntroText() {
+	var paragraph = [];
+
+	var longTimeAgo1 = "Em uma sala de aula na UFRJ,";
+	var longTimeAgo2 = "em pleno verao...";
+	paragraph.push(longTimeAgo1);
+	paragraph.push(longTimeAgo2);
+
+	for (var i = 0; i <= 1; i++) {
+		var text3d = new THREE.TextGeometry( paragraph[i], {
+			size: 20,
+			height: 1,
+			curveSegments: 5,
+			font: "helvetiker"
+		});
+
+		text3d.computeBoundingBox();
+		var centerOffset = -0.5 * ( text3d.boundingBox.max.x - text3d.boundingBox.min.x );
+		// overdraw is the width of the 3d letters
+		var textMaterial = new THREE.MeshBasicMaterial( { color: 'rgb(80, 241, 242)', overdraw: 0.1} );
+		// var textMaterial = new THREE.MeshLambertMaterial( { color: 0x50F1F2 } );
+		text = new THREE.Mesh( text3d, textMaterial );
+
+		text.position.x = centerOffset;
+		text.position.y = 150 - 30*i;
+		text.position.z = -100;
+
+		wholeText.push(text);
+
+		group = new THREE.Object3D();
+		group.add( text );
+		groups.push(group);
+		scene.add( group );
+	}
 }
 
 function onWindowResize() {
@@ -85,24 +268,39 @@ function onWindowResize() {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-function initBackground() {
+function animate() {
+	requestAnimationFrame( animate );
 
-    // Load the background texture
-    var texture        = THREE.ImageUtils.loadTexture('images/background.png');
-    
-    texture.wrapS	= THREE.RepeatWrapping;
-	texture.wrapT	= THREE.RepeatWrapping;
-    
-    var backgroundMesh = new THREE.Mesh(
-        new THREE.PlaneGeometry(2, 2, 0),
-        new THREE.MeshBasicMaterial({
-            map: texture
-        }));
+	render();
+	stats.update();
 
-    backgroundMesh.material.depthTest  = false;
-    backgroundMesh.material.depthWrite = false;
+	if (clock.getElapsedTime() >= 5 && groups.length > 0){
+		var size = groups.length;
+		for (var i = 0; i < size; i++){
+			// groups[i].children[0].material.opacity = 0;
+			scene.remove(groups[0]);
+			groups.splice(0, 1);
+		}
+	}
+}
 
-    // Create your background scene
-    backgroundScene.add(backgroundCamera);
-    backgroundScene.add(backgroundMesh);
+function render() {
+	renderer.autoClear = false;
+	renderer.clear();
+	// Makes the text go slower and disappear
+	// for(var i = 0; i < 6; i++){
+	//  	text.position.z -= i / 4
+	//  	text2.position.z -= i / 4
+	//  	text3.position.z -= i / 5
+	//  	text4.position.z -= i / 6
+	//  	text5.position.z -= i / 7
+	//  	text6.position.z -= i / 8
+	// }
+	// for(var i = 0; i < 6; i++){
+	// 	for (var i = 0; i < wholeText.length; i++) {
+	// 		wholeText[i].position.z -= i/4;
+	// 	}
+	// }
+
+	renderer.render( scene, camera );
 }

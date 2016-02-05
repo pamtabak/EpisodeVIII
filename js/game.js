e12000fd4b606919be56766744d1b7c98d247340
Eric Reis Figueiredo <eric_reisfig@poli.ufrj.br>
2016-02-05 17:54:08 -0200

    added space and space texture

--- js/game.js
+++ js/game.js
@@ -78,7 +78,7 @@ function createScene() {
     scene.clearColor = new BABYLON.Color3(0, 0, 1);
 
     // This creates and positions a free camera
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, -30, BABYLON.Vector3.Zero(), scene);
 
     // This targets the camera to scene origin
     camera.setTarget(BABYLON.Vector3.Zero());
@@ -92,16 +92,34 @@ function createScene() {
     // Dim the light a small amount
     light.intensity = .5;
 
-    // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+    // // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
+    // var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
 
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
+    // // Move the sphere upward 1/2 its height
+    // sphere.position.y = 1;
 
-    // Let's try our built-in 'ground' shape.  Params: name, width, depth, subdivisions, scene
-    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+    // // Let's try our built-in 'ground' shape.  Params: name, width, depth, subdivisions, scene
+    // var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
 
-    // Leave this function
-    return scene;
+    // create cylinder to represent the space
+    var spaceScale = 10.0;
+    var space = BABYLON.Mesh.CreateCylinder("space", 10 * spaceScale, 0, 6 * spaceScale, 20, 20, scene);
 
+    // create starfield shader to 
+    var starfieldPT = new BABYLON.StarfieldProceduralTexture("starfieldPT", 512, scene);
+    var starfieldMaterial = new BABYLON.StandardMaterial("starfield", scene);
+    starfieldMaterial.diffuseTexture = starfieldPT;
+    starfieldMaterial.diffuseTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    starfieldMaterial.backFaceCulling = false;
+    starfieldPT.beta = 0.1;
+
+    // put the material to the space mesh
+    space.material = starfieldMaterial;
+
+    // makes the space moves towards the spaceship
+    scene.registerBeforeRender(function () {
+        starfieldPT.time += scene.getAnimationRatio() * 0.8;
+    });
+
+   	return scene;	
 }

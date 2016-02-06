06f5d905204528febde25f4b8f2917fa215a57d3
Eric Reis Figueiredo <eric_reisfig@poli.ufrj.br>
2016-02-06 21:22:15 -0200

    initial movement

--- js/game.js
+++ js/game.js
@@ -9,6 +9,9 @@ var stats, container;
 
 var clock;
 var scene;
+var camera;
+var spaceship, planets, skybox;
+var moveLeft, moveRight, moveForward, moveBackward;
 
 document.addEventListener("DOMContentLoaded", function () { init(), animate(); }, false);
 
@@ -27,10 +30,21 @@ function init() {
 	createStats();
 
 	// initialize clock
-	clock = new THREE.Clock();  
+	clock = new THREE.Clock();
+
+	// initializing spaceship movement variables
+	moveLeft     = false;
+	moveRight    = false;
+	moveForward  = false;
+	moveBackward = false;  
+
+	spaceshipSpeed = 5.0;
+
+	initMovement();
 
 	// Watch for browser/canvas resize events
 	window.addEventListener("resize", function () { engine.resize(); });
+
 }
 
 function animate() {
@@ -73,7 +87,7 @@ function createScene() {
 
     scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
 
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 100, 150), scene);
+    camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 100, 150), scene);
     camera.setTarget(new BABYLON.Vector3(0, 0, -200));
     camera.attachControl(canvas, false);
 
@@ -103,21 +117,78 @@ function createSkybox (scene) {
 
 	// box + sky = skybox !
 	skybox.material = skyboxMaterial;
+
+	return skybox;
 }
 
 function createSpaceship (scene) {
 	// Load spaceship model
     BABYLON.SceneLoader.ImportMesh("", "assets/", "spaceship.babylon", scene, function (meshes) {
-    	var spaceship      = meshes[0];
+    	spaceship          = meshes[0];
 		spaceship.scaling  = new BABYLON.Vector3(0.08,0.08,0.08);
 		spaceship.position = new BABYLON.Vector3(0,0,0);
     });
 }
 
 function createPlanets (scene) {
+	// initializing return object
+	var planets      = [];
+
     var planet1      = BABYLON.Mesh.CreateSphere("planet1", 50.0, 100.0, scene);
     planet1.position = new BABYLON.Vector3(200, 100, -700);
+    planets.push(planet1);
 
     var planet2      = BABYLON.Mesh.CreateSphere("planet2", 50.0, 200.0, scene);
     planet2.position = new BABYLON.Vector3(-400, -500, -400);
+    planets.push(planet2);
+
+    return planets;
+}
+
+function initMovement() {
+	// When a key is pressed, set the movement
+    var onKeyDown = function(evt) {
+    	switch (evt.keyCode) {
+    		case 65:
+    			// key 'a' presseds
+	    		moveSpaceship(-spaceshipSpeed, 0);
+				break;  
+			case 68:
+    			// key 'd' pressed
+	    		moveSpaceship(spaceshipSpeed, 0);
+				break;
+			case 83:
+    			// key 's' presseds
+	    		moveSpaceship(0, -spaceshipSpeed);
+				break;  
+			case 87:
+    			// key 'w' pressed
+	    		moveSpaceship(0, spaceshipSpeed);
+				break; 
+    	}
+    };
+
+    var onKeyUp = function(evt) {
+    	moveLeft     = false;
+		moveRight    = false;
+		moveForward  = false;
+		moveBackward = false;
+    }
+
+	// Register events with the right Babylon function
+    BABYLON.Tools.RegisterTopRootEvents([{
+        name: "keydown",
+        handler: onKeyDown
+    }, {
+        name: "keyup",
+        handler: onKeyUp
+    }]);
+}
+
+function moveSpaceship(speedX, speedY) {
+	spaceship.position.x -= speedX;
+	spaceship.position.z -= speedY;
+
+	camera.position.x -= speedX;
+	camera.position.z -= speedY;
 }

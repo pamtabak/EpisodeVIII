e4e075a401d6541ba2748c5d999b4129cf43d8ff
Eric Reis Figueiredo <eric_reisfig@poli.ufrj.br>
2016-02-06 19:34:10 -0200

    spaceship and sky added

--- js/game.js
+++ js/game.js
@@ -10,116 +10,114 @@ var stats, container;
 var clock;
 var scene;
 
-init();
-animate();
+document.addEventListener("DOMContentLoaded", function () { init(), animate(); }, false);
 
 function init() {
 
-  container = document.createElement( 'div' );
-  document.body.appendChild( container );
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
 
-  // Now, call the createScene function that you just finished creating
-  scene = createScene();
-    // Register a render loop to repeatedly render the scene
-      engine.runRenderLoop(function () {
-        scene.render();
-    });
+	// Now, call the createScene function that you just finished creating
+	scene = createScene();
+	// Register a render loop to repeatedly render the scene
+	  engine.runRenderLoop(function () {
+	    scene.render();
+	});
 
-  createStats();
+	createStats();
 
-  // initialize clock
-  clock = new THREE.Clock();  
+	// initialize clock
+	clock = new THREE.Clock();  
 
-  // Watch for browser/canvas resize events
-  window.addEventListener("resize", function () { engine.resize(); });
+	// Watch for browser/canvas resize events
+	window.addEventListener("resize", function () { engine.resize(); });
 }
 
 function animate() {
-  requestAnimationFrame( animate );
+	requestAnimationFrame( animate );
 
-  render();
+	render();
 
-  stats.update();
+	stats.update();
 }
 
 function render() {
-  var elapsedTime = clock.getElapsedTime().toString().split(".")[0];
-  if (elapsedTime < 60) {
-    if (elapsedTime.length == 1)
-      elapsedTime = "0" + elapsedTime;
-    elapsedTime = "00:" + elapsedTime;
-  }
-  else {
-    var minutes = Math.floor(elapsedTime/60);
-    if (minutes.toString().length == 1)
-      minutes = "0" + minutes;
-    var seconds = elapsedTime%60;
-    if (seconds.toString().length == 1)
-      seconds = "0" + seconds;
-    elapsedTime = minutes + ":" + seconds;
-  }
-  divTime.innerHTML = elapsedTime;
+	var elapsedTime = clock.getElapsedTime().toString().split(".")[0];
+	if (elapsedTime < 60) {
+		if (elapsedTime.length == 1) { elapsedTime = "0" + elapsedTime; }
+		elapsedTime = "00:" + elapsedTime;
+	}
+	else {
+		var minutes = Math.floor(elapsedTime/60);
+		if (minutes.toString().length == 1) { minutes = "0" + minutes; }
+		var seconds = elapsedTime%60;
+		if (seconds.toString().length == 1) { seconds = "0" + seconds; }
+		elapsedTime = minutes + ":" + seconds;
+	}
+	
+	divTime.innerHTML = elapsedTime;
 
-  scene.render();
+	scene.render();
 }
 
 function createStats() {
-  stats = new Stats();
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.top = '0px';
-  container.appendChild( stats.domElement );
+	stats = new Stats();
+	stats.domElement.style.position = 'absolute';
+	stats.domElement.style.top      = '0px';
+	container.appendChild( stats.domElement );
 }
 
 function createScene() {
-    // Now create a basic Babylon Scene object 
+
     var scene = new BABYLON.Scene(engine);
 
-    // Change the scene background color to blue.
-    scene.clearColor = new BABYLON.Color3(0, 0, 1);
+    scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
 
-    // This creates and positions a free camera
-    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, -30, BABYLON.Vector3.Zero(), scene);
-
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
-
-    // This attaches the camera to the canvas
+    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 100, 150), scene);
+    camera.setTarget(new BABYLON.Vector3(0, 0, -200));
     camera.attachControl(canvas, false);
 
-    // This creates a light, aiming 0,1,0 - to the sky.
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
     // Dim the light a small amount
-    light.intensity = .5;
+    light.intensity = 0.5;
 
-    // // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
-    // var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-    // // Move the sphere upward 1/2 its height
-    // sphere.position.y = 1;
-
-    // // Let's try our built-in 'ground' shape.  Params: name, width, depth, subdivisions, scene
-    // var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-
-    // create cylinder to represent the space
-    var spaceScale = 10.0;
-    var space = BABYLON.Mesh.CreateCylinder("space", 10 * spaceScale, 0, 6 * spaceScale, 20, 20, scene);
-
-    // create starfield shader to 
-    var starfieldPT = new BABYLON.StarfieldProceduralTexture("starfieldPT", 512, scene);
-    var starfieldMaterial = new BABYLON.StandardMaterial("starfield", scene);
-    starfieldMaterial.diffuseTexture = starfieldPT;
-    starfieldMaterial.diffuseTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-    starfieldMaterial.backFaceCulling = false;
-    starfieldPT.beta = 0.1;
-
-    // put the material to the space mesh
-    space.material = starfieldMaterial;
-
-    // makes the space moves towards the spaceship
-    scene.registerBeforeRender(function () {
-        starfieldPT.time += scene.getAnimationRatio() * 0.8;
-    });
+    createSkybox(scene);
+    createSpaceship(scene);
+    createPlanets(scene);
 
    	return scene;	
+}
+
+function createSkybox (scene) {
+	// The box creation
+	var skybox = BABYLON.Mesh.CreateSphere("skyBox", 100.0, 10000.0, scene);
+
+	// The sky creation
+	var skyboxMaterial                               = new BABYLON.StandardMaterial("skyBox", scene);
+	skyboxMaterial.backFaceCulling                   = false;
+	skyboxMaterial.diffuseColor                      = new BABYLON.Color3(0, 0, 0);
+	skyboxMaterial.specularColor                     = new BABYLON.Color3(0, 0, 0);
+	skyboxMaterial.reflectionTexture                 = new BABYLON.CubeTexture("assets/space", scene);
+	skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+
+	// box + sky = skybox !
+	skybox.material = skyboxMaterial;
+}
+
+function createSpaceship (scene) {
+	// Load spaceship model
+    BABYLON.SceneLoader.ImportMesh("", "assets/", "spaceship.babylon", scene, function (meshes) {
+    	var spaceship      = meshes[0];
+		spaceship.scaling  = new BABYLON.Vector3(0.08,0.08,0.08);
+		spaceship.position = new BABYLON.Vector3(0,0,0);
+    });
+}
+
+function createPlanets (scene) {
+    var planet1      = BABYLON.Mesh.CreateSphere("planet1", 50.0, 100.0, scene);
+    planet1.position = new BABYLON.Vector3(200, 100, -700);
+
+    var planet2      = BABYLON.Mesh.CreateSphere("planet2", 50.0, 200.0, scene);
+    planet2.position = new BABYLON.Vector3(-400, -500, -400);
 }
